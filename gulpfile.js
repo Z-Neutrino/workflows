21e664f29e734b8521c28f325d77937810f69e1c
Tim <timbartrum73@gmail.com>
2018-03-07 16:46:46 +0000

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 
@@ -13,6 +14,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 
+// style includes all the files anyway so we don't need a bunch of files here
+var sassSources = ['components/sass/style.scss'];
 /*
   Notes:
   - When doing a gulp pipe you need to specify the original file location
@@ -38,4 +41,15 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
