e2e9f239ab86e871af65906c6f817cfdc451b9a1
Tim <timbartrum73@gmail.com>
2018-03-07 15:16:07 +0000

    Added Coffee Script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
-var gulp = require('gulp'); // Node js command to bring in the gulp library and assign to gulp variable
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
 /*
-The task function allows you to create a new task
-@param {string} = the name of your task
-@param {function} = the task to be performed
+  Notes:
+  - When doing a gulp pipe you need to specify the original file location
+  using the src method. This can be an array of files or a single file
+  - Then pipe it using the pipe method. Here we pass the coffee variable
+  through to specify the coffee module. We set an option as bare (see coffee
+  usage docs). Bare put a safety wrapper function around the JS.
+  It sends the output of the first plugin (coffee) to the input of the next
+  which is just the destination location - dest()
 */
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
