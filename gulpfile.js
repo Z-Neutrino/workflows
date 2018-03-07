95c4aba410514802ddf36c5655d35f677e0b44d1
Tim <timbartrum73@gmail.com>
2018-03-07 15:25:26 +0000

    Concatenated JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+
 /*
   Notes:
   - When doing a gulp pipe you need to specify the original file location
@@ -19,4 +27,13 @@ gulp.task('coffee', function() {
     .pipe(coffee({bare: true })
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
+});
+
+/*
+  Task to combine js files into one file
+*/
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
